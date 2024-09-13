959f0255f9673e3f4c1606e750a7853578845ead
k-ryll <andreskharyll@gmail.com>
2024-09-13 16:26:34 +0800

    added css split to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', 
   plugins: [react()],
   define: {
     'process.env': process.env,
   },
+  build: {
+    cssCodeSplit: true, // Split CSS into separate files for better caching
+    minify: 'esbuild', // Ensure minification happens
+  }
 })
